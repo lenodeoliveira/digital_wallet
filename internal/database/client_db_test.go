897d4bdbdb875d4d45b460a67c01ce63599c9120
J Lennon S. De Oliveira <lenodeoliveira@gmail.com>
2023-08-27 18:53:06 -0300

    feat: finalizing digital wallet project

--- internal/database/client_db_test.go
+++ internal/database/client_db_test.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com.br/lenodeoliveira/fc-ms-wallet/internal/entity"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -29,4 +31,26 @@ func (s *ClientDBTestSuite) TearDownSuite() {
 
 func TestClientDBTestSuite(t *testing.T) {
 	suite.Run(t, new(ClientDBTestSuite))
+}
+
+func (s *ClientDBTestSuite) TestGet() {
+	client, _ := entity.NewClient("John", "john@mail.com")
+	s.clientDB.Save(client)
+
+	clientDB, err := s.clientDB.Get(client.ID)
+	s.Nil(err)
+	s.Equal(client.ID, clientDB.ID)
+	s.Equal(client.Name, clientDB.Name)
+	s.Equal(client.Email, clientDB.Email)
+}
+
+func (s *ClientDBTestSuite) TestSave() {
+	client := &entity.Client{
+		ID:    "1",
+		Name:  "Test",
+		Email: "test@mail.com",
+	}
+
+	err := s.clientDB.Save(client)
+	s.Nil(err)
 }
